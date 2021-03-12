0967995870d81420b8d39becb54d080bad85093b
SERKAN <serkanbircan21@yandex.com>
2021-03-12 14:28:39 +0300

    removed font-sans

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
     return (
-        <div className="font-sans min-h-screen px-2 flex flex-col items-center">
+        <div className="min-h-screen px-2 flex flex-col items-center">
             <Head>
                 <title>Nexy JS Template</title>
                 <link rel="icon" href="/favicon.ico" />

455634f9d56132d6a3873a2522c208f4c7106573
SERKAN <serkanbircan21@yandex.com>
2021-03-18 13:34:17 +0300

    added font-face for inter font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,18 @@ class Document extends NextDocument {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <link
+                        rel="preload"
+                        href="/fonts/inter-var-latin.woff2"
+                        as="font"
+                        type="font/woff2"
+                        crossOrigin="anonymous"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta content="#ffffff" name="theme-color" />
+                    <meta content="#ffffff" name="msapplication-TileColor" />
+                </Head>
 
                 <body className="font-sans">
                     <Main />

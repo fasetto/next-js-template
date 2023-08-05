50e06ab04799bb16413763924c0003527c885326
Serkan <serkanbircan21@yandex.com>
2023-08-05 16:07:02 +0300

    Update template

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 
 import Button from "./Button";
 
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: "Button",
   component: Button,
-} as ComponentMeta<typeof Button>;
+} as Meta<typeof Button>;
 
-//👇 We create a “template” of how args map to rendering
-const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
+type Story = StoryObj<typeof Button>;
 
-//👇 Each story then reuses that template
-export const Primary = Template.bind({});
-Primary.args = {
-  children: "Button",
-  variant: "primary",
+export const Primary: Story = {
+  args: {
+    appearance: "primary",
+    children: "Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    appearance: "secondary",
+    children: "Button",
+  },
 };
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,32 +1,27 @@
 import { ButtonHTMLAttributes } from "react";
-import clsx from "clsx";
+import { VariantProps, tv } from "tailwind-variants";
 
-type Props = {
-  variant?: keyof typeof variantStyles;
-  children?: React.ReactNode;
-};
+const button = tv({
+  base: "px-3 py-2 rounded-md text-white text-sm font-medium",
+  variants: {
+    appearance: {
+      primary: "bg-indigo-600 hover:bg-indigo-700",
+      secondary: "bg-indigo-50 hover:bg-indigo-100 text-indigo-600",
+    },
+  },
+  defaultVariants: {
+    appearance: "primary",
+  },
+});
 
-const variantStyles = {
-  primary: "bg-indigo-600 hover:bg-indigo-700",
-  secondary: "bg-indigo-50 hover:bg-indigo-100 text-indigo-600",
-};
+type Props = VariantProps<typeof button> &
+  React.ComponentPropsWithoutRef<"button">;
 
 const Button = ({
-  variant = "primary",
-  children,
+  appearance,
   ...rest
 }: Props & ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return (
-    <button
-      className={clsx(
-        "px-3 py-2 rounded-md text-white text-sm font-medium",
-        variantStyles[variant]
-      )}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+  return <button className={button({ appearance })} {...rest} />;
 };
 
 export default Button;
